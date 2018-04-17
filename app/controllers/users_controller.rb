6474f3ea8d5d4d1c8ccdba06acf6cdd2eb49e4ce
Damien le <mycauhoi@gmail.com>
2018-04-17 02:59:34 -0400

    adding ajax call to delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+
   def index
-    @users = User.all
+    @users = User.all.includes(:company)
   end
 
   def create
@@ -10,6 +11,16 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @id = user.id
+    user.destroy
+    respond_to do |format|
+      format.js {}
+    end
+  end
+
+  def remove_all
+    User.delete_all
+    redirect_to users_path
   end
 
   def show
@@ -23,6 +34,7 @@ class UsersController < ApplicationController
     uploaded_file = params[:user_csv]
     if uploaded_file.nil?
       flash[:warning] = "Please select a csv file"
+      redirect_to users_bulk_upload_path and return
     end
     arr = load_from_csv(uploaded_file)
     errors = arr.map do |row|
@@ -32,9 +44,9 @@ class UsersController < ApplicationController
    if errors.present?
      if errors.length > 5
        error_message = errors.take(5).map {|e| CGI::escapeHTML(e)}.join('</br>')
-       flash[:error] = "#{error_message} <br>There are more than 5 errors when importing users. Please see the log".html_safe
+       flash[:error] = "#{error_message} <br>There are more than 5 errors when importing users. Please see the log for details".html_safe
      else
-       flash[:error] = errors.take(5).map {|e| CGI::escapeHTML(e)}.join('</br>').html_safe
+       flash[:error] = errors.map {|e| CGI::escapeHTML(e)}.join('</br>').html_safe
      end
      redirect_to users_bulk_upload_path
    else
@@ -46,6 +58,10 @@ class UsersController < ApplicationController
 
   end
 
+private
+  def user
+      @user||= User.find_by(id: params[:id])
+  end
 
 
 end
