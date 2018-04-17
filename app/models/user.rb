cacf42f4a1c1acb2b201202bd25585d69355edfe
Damien le <mycauhoi@gmail.com>
2018-04-17 03:01:35 -0400

    adding error handling and update view with bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,18 @@
 class User < ApplicationRecord
   belongs_to :company
+  validates_presence_of :email_address
 
   def self.create_user_from_hash(user_hash)
     error = nil
     begin
+      fn = user_hash[:first_name]
+      ln = user_hash[:last_name]
+      email = user_hash[:email_address]
+      phone = user_hash[:phone_number]
+      company = user_hash[:company_name]
       User.transaction do
-        fn = user_hash[:first_name]
-        ln = user_hash[:last_name]
-        email = user_hash[:email_address]
-        phone = user_hash[:phone_number]
-        company = user_hash[:company_name]
-        if user_company = Company.find_or_create_by(name: company)
+
+        if company.present? && (user_company = Company.find_or_create_by(name: company))
           if user = User.find_by(email_address: email)
             error = "User #{email} is already exists"
             Rails.logger.error(error)
@@ -19,14 +21,16 @@ class User < ApplicationRecord
             Rails.logger.info("User #{email} is imported")
           end
         else
-          error = "Company Name Field is empty. User is not created"
+          error = "Company Name Field is empty. User #{fn} is not created"
           Rails.logger.error(error)
         end
       end
       error
-    rescue => e
+    rescue ActiveRecord::RecordInvalid => invalid
+      error = "Error importing user #{fn} - " + invalid.record.errors.full_messages.first
+    rescue  => e
       Rails.logger.error("Error create user #{fn} - email #{email}")
-      error = "Error importing user #{email}"
+      error = "Error importing user #{email} #{e.message}"
       error
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'users/bulk_upload'
+  post 'users/remove_all'
   post 'users/upload'
   resources :users
-
+  root 'users#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
