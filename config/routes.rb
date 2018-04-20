9659e482ba3864975017b655b68012d9266361a6
Damien le <mycauhoi@gmail.com>
2018-04-20 01:23:39 -0400

    set default route to delete as js to fix the ajax issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/bulk_upload'
   post 'users/remove_all'
   post 'users/upload'
+  delete '/users/:id' => 'users#destroy', :as => :delete_user, defaults: {format: :js}
   resources :users
   root 'users#index'
 
